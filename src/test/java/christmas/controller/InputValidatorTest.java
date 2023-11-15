ef737aabe848d4367fa225070036b1755d26777d
JW0321 <digneotn123@gmail.com>
2023-11-15 20:50:14 +0900

    test: 날짜 입력 범위 예외 테스트 구현

--- src/test/java/christmas/controller/InputValidatorTest.java
+++ src/test/java/christmas/controller/InputValidatorTest.java
@@ -12,9 +12,16 @@ public class InputValidatorTest {
     @DisplayName("날짜 입력 검증 예외")
     @ValueSource(strings = {"", " ", "ab", "-1", "!"})
     void validateNumberTest(String input) {
-
         assertThatThrownBy(() -> InputValidator.validateNumber(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("날짜 입력 범위 예외")
+    @ValueSource(ints = {0, 32, 99})
+    void checkNumberInRangeTest(int input) {
+    assertThatThrownBy(() -> InputValidator.checkNumberInRange(input))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
