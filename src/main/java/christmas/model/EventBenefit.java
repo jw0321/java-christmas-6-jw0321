a656eb25a84688a3f79ef0725a14ff6065bdbe0b
JW0321 <digneotn123@gmail.com>
2023-11-15 16:50:27 +0900

    feat: 할인전 주문 금액에 따른 증정 기능 구현

---
+++ src/main/java/christmas/model/EventBenefit.java
@@ -0,0 +1,33 @@
+package christmas.model;
+
+import christmas.model.Order.FoodCategory;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EventBenefit { //이벤트 배지 부여 및 증정 메뉴 로직 처리
+
+    private final String GIFT_MENU = "샴페인";
+    private final Map<String, Integer> gift;
+
+    public EventBenefit() {
+        this.gift = new HashMap<>();
+    }
+
+    public void setGift(int rawOrderAmount) {
+        int givingGiftByAmount = 120000;
+        if (rawOrderAmount >= givingGiftByAmount) {
+            int giftPrice = FoodCategory.BEVERAGE.getFoodMenu().get(GIFT_MENU);
+            this.gift.put(GIFT_MENU, giftPrice);
+        }
+    }
+
+    public Map<String, Integer> getGiftDetails() {
+        Map<String, Integer> giftReceipt = new HashMap<>();
+        int quantity = 1;
+
+        giftReceipt.put(GIFT_MENU, quantity);
+
+        return giftReceipt;
+    }
+
+}
