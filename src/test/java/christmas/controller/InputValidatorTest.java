eb1269d52537c8a4d7460c2d3600288b338dd486
JW0321 <digneotn123@gmail.com>
2023-11-15 21:22:25 +0900

    test: 메뉴 문자 검증 기능 테스트 구현

--- src/test/java/christmas/controller/InputValidatorTest.java
+++ src/test/java/christmas/controller/InputValidatorTest.java
@@ -2,8 +2,13 @@ package christmas.controller;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Map;
+import java.util.stream.Stream;
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputValidatorTest {
@@ -24,4 +29,21 @@ public class InputValidatorTest {
             .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @ParameterizedTest
+    @DisplayName("메뉴 입력 형식 검증 예외")
+    @MethodSource("validateOrderDataProvider")
+    void validateOrderTest(Map<String, Integer> separatedOrder, String[] keyValue) {
+        assertThatThrownBy(() -> InputValidator.validateOrder(separatedOrder, keyValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> validateOrderDataProvider() {
+        return Stream.of(
+                Arguments.of(Map.of("메뉴", 1), new String[]{"한덩어리"}),
+                Arguments.of(Map.of("메뉴", 1), new String[]{"세","덩","어리"}),
+                Arguments.of(Map.of("중복메뉴", 1), new String[]{"중복메뉴", "1"}),
+                Arguments.of(Map.of("129", 1), new String[]{"2312", "3"}),
+                Arguments.of(Map.of("메뉴", 1), new String[]{"문자수량", "a"})
+        );
+    }
 }
