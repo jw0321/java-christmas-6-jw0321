a2c9cf85487640e28b2b68263034ec8bb3ad2d32
JW0321 <digneotn123@gmail.com>
2023-11-15 22:01:39 +0900

    refactor: 안쓰는 import문 제거

--- src/test/java/christmas/controller/InputValidatorTest.java
+++ src/test/java/christmas/controller/InputValidatorTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
