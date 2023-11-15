5a447bc6f595e1413155d5cdae279ddefe6c9961
JW0321 <digneotn123@gmail.com>
2023-11-15 20:33:13 +0900

    test: 주문 입력 기능 및 예외 테스트 구현

---
+++ src/test/java/christmas/model/OrderTest.java
@@ -0,0 +1,49 @@
+package christmas.model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class OrderTest {
+
+    private Order order;
+
+    @BeforeEach
+    void setUp() {
+        this.order = new Order();
+    }
+
+    @DisplayName("주문 입력 테스트")
+    @Test
+    void setOrderTest() {
+        Map<String, Integer> inputOrder = new HashMap<>(Map.of("티본스테이크",1, "제로콜라",3));
+
+        order.setOrder(inputOrder);
+
+        assertThat(order.getOrderReceipt().details().keySet()).contains("티본스테이크");
+    }
+
+    @ParameterizedTest
+    @DisplayName("주문 입력 예외 테스트")
+    @MethodSource("setOrderExceptionDataProvider")
+    void setOrderExceptionTest(Map<String, Integer> inputOrder) {
+        assertThatThrownBy(() -> order.setOrder(inputOrder))
+                        .isInstanceOf(IllegalArgumentException.class);
+    }
+    private static Stream<Arguments> setOrderExceptionDataProvider() {
+        return Stream.of(
+                Arguments.of(Map.of("갈릭스테이크",1)),
+                Arguments.of(Map.of("티본스테이크", -1)),
+                Arguments.of(Map.of("티본스테이크", 21)),
+                Arguments.of(Map.of("티본스테이크", 10, "제로콜라", 15))
+        );
+    }
+}
